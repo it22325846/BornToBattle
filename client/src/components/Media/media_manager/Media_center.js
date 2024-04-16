3fca3c4cb645f59c6b473f3bb3d8c71070df39fe
Vishmitha Nipun <it22336354@my.sliit.lk>
2024-04-16 21:18:28 +0530

    style media_center update

--- client/src/components/Media/media_manager/Media_center.js
+++ client/src/components/Media/media_manager/Media_center.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./styles/MCenter.css";
+import "../styles/MCenter.css";
 
 export default function MediaCenter() {
   return (
