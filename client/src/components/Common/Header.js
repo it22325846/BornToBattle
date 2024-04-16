066d47ea83af03824f7b9338e22751b59d043637
IT22309624 <129936797+IT22309624@users.noreply.github.com>
2024-04-17 00:03:23 +0530

    .

--- client/src/components/Common/Header.js
+++ client/src/components/Common/Header.js
@@ -11,7 +11,7 @@ function Header() {
         className="image_nav"
         style={{ marginRight: "auto", marginLeft: "10px" }}
       >
-        <a href="home">
+        <a href="/">
           <img src="../../Images/ids.png" alt="IDS" width="60" height="40" />
         </a>
       </div>
