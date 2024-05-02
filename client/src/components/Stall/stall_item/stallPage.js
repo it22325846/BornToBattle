31ebdc4a929e2bfe3b86dd068ba855839eb09588
N-isith <it22361936@my.sliit.lk>
2024-05-02 20:04:41 +0530

    completed pdf generate

--- client/src/components/Stall/stall_item/stallPage.js
+++ client/src/components/Stall/stall_item/stallPage.js
@@ -15,6 +15,7 @@ export default function UserStalls() {
             axios.post("http://localhost:8020/staller/items/read")
                 .then((res) => {
                     setItems(res.data);
+                    console.log(res.data)
                 })
                 .catch((err) => {
                     alert(err.message);
@@ -25,7 +26,7 @@ export default function UserStalls() {
 
     const handlePrint = (event) => {
         event.preventDefault();
-        generatePDF(); 
+        generatePDF();
     }
 
     const generatePDF = useReactToPrint({
@@ -45,35 +46,37 @@ export default function UserStalls() {
 
 
             <div className='container rounded'>
-                <div className='row' >
-                    {items.filter((item) => {
-                        return search.toLowerCase() === ''
-                            ? item
-                            : item.pName.toLowerCase().includes(search);
-                    }).map((item) => (
-                        <div className='col-md-6' key={item._id}>
-                            <div className='d-flex flex-column' style={{ position: 'relative' }}>
-                                <div className='bg-white' style={{ marginInline: '70px' }}>
-                                    <div className='card bg-white rounded-0'>
-                                        <div className='card-block'>
-                                            {item.pImage &&
-                                                <img
-                                                    src={`http://localhost:8020/staller/items/uploads/${item.pImage}`}
-                                                    alt="Product Image"
-                                                    style={{ maxWidth: '300px', height: '300px', alignSelf: 'center' }}
-                                                />}
-                                        </div>
-                                        <div className='card-block'>
-                                            <h3 className='card-text bg-white text-dark rounded-bottom' >{item.pName}</h3>
-                                        </div>
-                                        <div className='card-block my-3'>
-                                            <h3 className='card-text text-white rounded-4 px-4' style={{ display: 'inline-block', backgroundColor: '#ff0000' }}>Rs.{item.pPrice}</h3>
+                <div ref={componentPDF} style={{ width: '100%' }}>
+                    <div className='row' >
+                        {items.filter((item) => {
+                            return search.toLowerCase() === ''
+                                ? item
+                                : item.pName.toLowerCase().includes(search);
+                        }).map((item) => (
+                            <div className='col-md-6' key={item._id}>
+                                <div className='d-flex flex-column' style={{ position: 'relative' }}>
+                                    <div className='bg-white' style={{ marginInline: '70px' }}>
+                                        <div className='card bg-white rounded-0'>
+                                            <div className='card-block'>
+                                                {item.pImage &&
+                                                    <img
+                                                        src={`http://localhost:8020/staller/items/uploads/stallItems/${item.pImage}`}
+                                                        alt="Product Image"
+                                                        style={{ maxWidth: '300px', height: '300px', alignSelf: 'center' }}
+                                                    />}
+                                            </div>
+                                            <div className='card-block'>
+                                                <h3 className='card-text bg-white text-dark rounded-bottom' >{item.pName}</h3>
+                                            </div>
+                                            <div className='card-block my-3'>
+                                                <h3 className='card-text text-white rounded-4 px-4' style={{ display: 'inline-block', backgroundColor: '#ff0000' }}>Rs.{item.pPrice}</h3>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
