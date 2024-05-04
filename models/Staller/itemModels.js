2561ab3e8d971b0e8220109ede26e65e382a6c68
N-isith <it22361936@my.sliit.lk>
2024-05-04 18:20:03 +0530

    validate  items

--- models/Staller/itemModels.js
+++ models/Staller/itemModels.js
@@ -1,8 +1,16 @@
 //because we connect with mongoDB
 const mongoose = require('mongoose');
+const yup = require('yup');
 
 //create the schema variable
 const schema = mongoose.Schema;
+
+// Define a Yup schema for validation
+const ItemSchemaValidation = yup.object().shape({
+    pName: yup.string().min(5).max(20).required(),
+    pPrice: yup.string().positive().lessthan(500).required(), 
+    pImage: yup.string().required(),
+});
 
 //create the schema object
 const itemSchema = new schema({
@@ -20,6 +28,17 @@ const itemSchema = new schema({
     },
     
 })
+
+// Add a pre-save hook to validate the data before saving it to the database
+ItemSchemaValidation.pre('save', async function(next) {
+    try {
+        await ItemSchemaValidation.validate(this.toObject());
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 //pass the created schema to the mongoDB
 //                           <document name>
