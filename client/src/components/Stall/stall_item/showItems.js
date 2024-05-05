fdb11a7b69bab2a86c7f876cd8062269d5ad538a
N-isith <it22361936@my.sliit.lk>
2024-05-05 15:19:45 +0530

    styled items pages

--- client/src/components/Stall/stall_item/showItems.js
+++ client/src/components/Stall/stall_item/showItems.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../../Style/stallStyles/EditStallStyles.css';
 import { useNavigate } from 'react-router-dom';
+import { useReactToPrint } from 'react-to-print';
 
 export default function ItemShow() {
   const [items, setItems] = useState([]);
   const navigate = useNavigate();
   const [search, setSearch] = useState("");
-
+  const componentPDF = useRef();
 
 
   const goToEditItem = (Itemid) => {
@@ -40,55 +41,75 @@ export default function ItemShow() {
   }
 
 
+
+
+  const handlePrint = (event) => {
+    event.preventDefault();
+    generatePDF();
+  }
+
+  const generatePDF = useReactToPrint({
+    content: () => componentPDF.current,
+    documentTitle: "StallItemData"
+  });
+
   return (
-    <div className='fullDiv rounded-4'>
-      <h2>Items</h2>
+    <div className='fullDiv rounded-4 mt-5 mb-5' style={{ borderRadius: '30px', marginInline: '100px' }}>
+      <h1 className='header my-3' style={{ marginLeft: '6.2in' }}>Items</h1>
+
       <form>
-        <input className='form-control' placeholder='Search items...' onChange={(e) => { setSearch(e.target.value) }} />
+        <div className='row' style={{ marginLeft: '2cm' }}>
+          <input className='form-control' style={{ width: '10.3in' }} placeholder='Search items...' onChange={(e) => { setSearch(e.target.value) }} />
+          <button className='btn btn-success' style={{ marginLeft: '20px', marginTop: '-10px' }} onClick={handlePrint}>Download PDF</button>
+        </div>
       </form>
 
-      <div className='container'>
-        <div className='row'>
-          {items.filter((item) => {
-            return search.toLowerCase() === ''
-              ? item
-              : item.pName.toLowerCase().includes(search);
-          }).map((item) => (
-            item && ( // Check if item is not null
+
+      <div className='container rounded-4 p-4' style={{ opacity: '75%' }}>
+        <div ref={componentPDF} style={{ width: '100%' }}>
+          <div className='row' >
+            {items.filter((item) => {
+              return search.toLowerCase() === ''
+                ? item
+                : item.pName.toLowerCase().includes(search);
+            }).map((item) => (
               <div className='col-md-6' key={item._id}>
                 <div className='d-flex flex-column' style={{ position: 'relative' }}>
-                  <div className='d-flex flex-column gap-2' style={{ position: 'absolute', top: '0px', right: '15px', zIndex: 2 }}> {/* Button container */}
-                    <button className='btn rounded-circle border border-4 edit-delete-btn' onClick={() => goToEditItem(item._id)}>
+                  <div className='d-flex flex-column gap-2 mt-4' style={{ position: 'absolute', top: '0px', right: '15px', zIndex: 2 }}> {/* Button container */}
+                    <button className='btn btn-success text-white' onClick={() => goToEditItem(item._id)}>
                       edit
                     </button>
-                    <button className='btn rounded-circle border border-4 edit-delete-btn' onClick={(d) => deleteItem(item._id)}>
+                    <button className='btn btn-warning text-white mt-1' onClick={(d) => deleteItem(item._id)}>
                       delete
                     </button>
                   </div>
-                  <div className='bg-white mb-5' style={{ marginInline: '70px' }}>
-                    <div className='card'>
+                  <div className='bg-white ' style={{ marginInline: '90px', borderRadius: '50px' }}>
+                    <div className='card bg-white' style={{ borderRadius: '25px' }}>
                       <div className='card-block'>
-                        <h3 className='card-title bg-dark text-white' >{item.pName}</h3>
-                      </div>
-                      <div>
-                        {item.pImage && (
+                        {item.pImage &&
                           <img
                             src={`http://localhost:8020/staller/items/uploads/stallItems/${item.pImage}`}
                             alt="Product Image"
                             style={{ maxWidth: '300px', height: '300px', alignSelf: 'center' }}
-                          />)}
+                          />}
                       </div>
                       <div className='card-block'>
-                        <p className='card-text bg-dark text-white'>{item.pPrice}</p>
+                        <h3 className='card-text bg-white text-dark rounded-bottom' >{item.pName}</h3>
+                      </div>
+                      <div className='card-block my-3'>
+                        <h3 className='card-text text-white rounded-4 px-4' style={{ display: 'inline-block', backgroundColor: '#ff0000' }}>Rs.{item.pPrice}</h3>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            )
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
 }
+
+
+
