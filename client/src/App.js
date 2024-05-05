dcfc678cbf26701088feb4a9fe6cc138d730f200
Vishmitha Nipun <it22336354@my.sliit.lk>
2024-05-05 23:54:32 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -183,6 +183,7 @@ import UpdateSponsor from './components/sponsor/updateSponsor';
 // #endregion
 
 //#region Vishmitha
+import mediaManagerUI from './components/Media/media_manager/mediaManagerUI';
 
 
 // #endregion
@@ -402,6 +403,8 @@ function App() {
         </>
 
         <>{/* Vishmitha registration routes */}
+        <Route path="/mediaM" element={<mediaManagerUI />} />
+
         </>
 
          
