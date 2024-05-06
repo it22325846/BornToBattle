a741584dda32dd76054cf99ce5bbf51cc92078c8
it22317780 <it22317780@my.sliit.lk>
2024-05-06 12:24:45 +0530

    g

--- client/src/components/Common/Header.js
+++ client/src/components/Common/Header.js
@@ -58,7 +58,7 @@ function Header() {
       <span className="separator">|</span>
 
       <li className="nav-item">
-        <a className="nav-link" href="displayevents">
+        <a className="nav-link" href="schedule">
           EVENTS
         </a>
       </li>
